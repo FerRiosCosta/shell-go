99aa160b1ff69bf62a0ea944ac1f36738055c9b1
Fernando Rios <fernandorios@Fernandos-MacBook-Pro.local>
2026-03-15 09:46:05 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,6 +12,10 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
-	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input", err)
+		os.Exit(1)
+	}
 	fmt.Println(command[:len(command)-1] + ": command not found")
 }
